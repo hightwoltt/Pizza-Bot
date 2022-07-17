21a9d34cdc3fa60aa7f70cea88e306528fdac465
Вадим Солонцов <vadim.solontsov@mail.ru>
2022-07-18 02:09:40 +0600

    Added h1 tag

---
+++ bot_telegram.py
@@ -0,0 +1,94 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import os
+import datetime
+
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+
+dt = datetime.datetime.now()
+
+dt_string_format = dt.strftime("Date %d.%m.%Y time: %H:%M:%S")
+
+
+async def on_startup(_):
+    print(f'\nБот запущен кореектно\n\nstatus - Online\ntime statted - {dt_string_format}')
+
+''' ======================== CLIENT PART ======================== '''
+
+@dp.message_handler(commands=['start','help'])
+async def commands_start(message: types.Message):
+
+    try:
+        await bot.send_message(message.from_user.id,  'Приятного аппетита')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, наишите ему:\nhttps//t.me/TODO-BRO-BOT')
+
+
+@dp.message_handler(commands=['Режим_работы'])
+async def pizza_open_command(message: types.Message):
+    
+    await bot.send_message(message.from_user.id, ' Пн-Пт 9:00 - 22:00 \nСб-Вс 9:00 - 02:00')
+
+
+@dp.message_handler(commands=['Адрес'])
+async def pizza_place_command(message: types.Message):
+    
+    await bot.send_message(message.from_user.id, 'Восток-2; Дом-5; Кв-27')
+
+
+
+        
+''' ======================== ADMIM PART ======================== '''
+
+
+
+''' ======================== GENERAL PART ======================== '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+executor.start_polling(
+    dp, skip_updates=True,
+    on_startup=on_startup
+)
