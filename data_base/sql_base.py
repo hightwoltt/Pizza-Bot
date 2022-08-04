8544a7937c1d6245ac56585885709c2da631c8f8
Вадим Солонцов <vadim.solontsov@mail.ru>
2022-08-05 00:15:04 +0600

    +

--- data_base/sql_base.py
+++ data_base/sql_base.py
@@ -2,7 +2,7 @@ from create_bot import bot, dp
 
 import sqlite3 as sq
 
-
+# Create a database table
 def sql_start():
     global base, cur
     base = sq.connect('3,14zza.db')
@@ -13,12 +13,13 @@ def sql_start():
     base.execute('CREATE TABLE IF NOT EXISTS menu(img TEXT, name TEXT PRIMARY KEY, description TEXT, price TEXT)')
     base.commit()
 
-
+# Add a insert command
 async def sql_add_command(state):
     async with state.proxy() as data:
         cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?)', tuple(data.values()))
         base.commit()
 
+# Add a menue output command
 async def sql_read(message):
     for position in cur.execute('SELECT * FROM menu').fetchall():
         await bot.send_photo(message.from_user.id, position[0],
